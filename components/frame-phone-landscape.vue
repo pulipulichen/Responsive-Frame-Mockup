d89eb3e6cdf5ab0b226341779d0dbd425e85e005
Pulipuli Chen <blog@pulipuli.info>
2020-11-28 01:16:22 +0800

    1125 哦哦哦，放上去了

--- components/frame-phone-landscape.vue
+++ components/frame-phone-landscape.vue
@@ -2,17 +2,12 @@
   <div class="frame-phone-landscape frame" 
        v-bind:class="{'show-notification-bar': $parent.deviceDisplayNotificationBar}">
 
-     <div class="frame-top-container">
-       <img src="img/frames/phone-portrait/top.png" />
+     <div class="frame-left-container">
+       <img src="img/frames/phone-landscape/top2left.png" />
      </div>
 
      <div class="frame-image-background-container">
-       <div class="frame-left">
-         <div class="frame-image-container">
-           <img v-bind:src="$parent.imgSrc" class="frame-image" v-bind:class="{'is-landscape': $parent.imgIsLandscape}" />
-         </div>
-       </div>
-       <div class="frame-right">
+       <div class="frame-top">
          <div class="frame-image-container">
            <img v-bind:src="$parent.imgSrc" class="frame-image" v-bind:class="{'is-landscape': $parent.imgIsLandscape}" />
          </div>
@@ -25,11 +20,17 @@
            <img v-bind:src="$parent.imgSrc" class="frame-image" v-bind:class="{'is-landscape': $parent.imgIsLandscape}" />
          </div>
        </div>
+       
+       <div class="frame-bottom">
+         <div class="frame-image-container">
+           <img v-bind:src="$parent.imgSrc" class="frame-image" v-bind:class="{'is-landscape': $parent.imgIsLandscape}" />
+         </div>
+       </div>
 
      </div>
 
-     <div class="frame-bottom-container">
-       <img src="img/frames/phone-portrait/bottom.png" />
+     <div class="frame-right-container">
+       <img src="img/frames/phone-landscape/bottom2right.png" />
      </div>
    </div>
 </template>
